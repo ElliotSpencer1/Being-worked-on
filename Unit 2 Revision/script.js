0ecc15574db5bcec07427fa9e259d6c8612ae6f9
Elliot Spencer <135474191+ElliotSpencer1@users.noreply.github.com>
2024-11-18 09:14:59 +0000

    index.html

---
+++ Unit 2 Revision/script.js
@@ -0,0 +1,5 @@
+function startRevision() {
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('title').innerText = 'Your Question Here';
+    document.getElementById('questionContainer').classList.remove('hidden');
+}
