434cbff2aef169be0607f71d9ed9b8c70cecbbd4
Elliot Spencer <135474191+ElliotSpencer1@users.noreply.github.com>
2025-01-15 09:03:54 +0000

    script.js

---  MetroidVania Unit15/scripts/script.js
+++  MetroidVania Unit15/scripts/script.js
@@ -1,8 +1,14 @@
+// map locations
+let tutorial;
+// characters
 let player;
 
 function setup(){
     createCanvas(windowWidth, windowHeight)
 
+    player = new sprite(windowWidth/2, windowHeight/2, 15,15, "s");
+    player.color = "blue";
+    
 }
 
 
