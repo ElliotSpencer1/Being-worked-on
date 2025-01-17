aa3e38e708d7c8c9391178182eeccf8bdcae2094
Elliot Spencer <135474191+ElliotSpencer1@users.noreply.github.com>
2025-01-17 14:49:59 +0000

    script.js

---  MetroidVania Unit15/scripts/script.js
+++  MetroidVania Unit15/scripts/script.js
@@ -1,10 +1,104 @@
-let beancoin
+let player;
 
+// hud
+let upperbar;
+
+// defining inner map variables
+let tilesize;
+let backdrop, floor, stone, mudpile, enemy, boss, door, bossdoor, origin, walls, lootroom, otherfloor;
+
+// defining map variables
 let mapA, mapB, mapC, mapD;
 
 function maps(){
+    tilesize = 10;
+
+    lootroom = new Group();
+    lootroom.w = tilesize;
+    lootroom.h = tilesize;
+    lootroom.tile = "y";
+    lootroom.color = "gold";
+    lootroom.collider = "s";
+
+    walls = new Group();
+    walls.w = tilesize;
+    walls.h = tilesize;
+    walls.tile = "t";
+    walls.color = "purple";
+    walls.collider = "s";
+
+    origin = new Group();
+    origin.w = tilesize;
+    origin.h = tilesize;
+    origin.tile = "O";
+    origin.color = "red";
+    origin.collider = "s";
+
+    backdrop = new Group();
+    backdrop.w = tilesize;
+    backdrop.h = tilesize;
+    backdrop.color = "black";
+    backdrop.tile = "b";
+    backdrop.collider = "n";
+
+    otherfloor = new Group();
+    otherfloor.w = tilesize;
+    otherfloor.h = tilesize;
+    otherfloor.color = "beige";
+    otherfloor.tile = "p";
+    otherfloor.collider = "n";
+
+    floor = new Group();
+    floor.w = tilesize;
+    floor.h = tilesize;
+    floor.color = "beige";
+    floor.tile = "H";
+    floor.collider = "n";
+
+    stone = new Group();
+    stone.w = tilesize;
+    stone.h = tilesize;
+    stone.color = "gray";
+    stone.tile = "S";
+    stone.collider = "n";
+
+    mudpile = new Group();
+    mudpile.w = tilesize;
+    mudpile.h = tilesize;
+    mudpile.color = "brown";
+    mudpile.tile = "m";
+    mudpile.collider = "n";
+
+    enemy = new Group();
+    enemy.w = tilesize;
+    enemy.h = tilesize;
+    enemy.color = "crimson";
+    enemy.tile = "r";
+    enemy.collider = "n";
+
+    boss = new Group();
+    boss.w = tilesize;
+    boss.h = tilesize;
+    boss.color = "green";
+    boss.tile = "B";
+    boss.collider = "n";
+
+    door = new Group();
+    door.w = tilesize;
+    door.h = tilesize;
+    door.color = "brown";
+    door.tile = "D";
+    door.collider = "n";
+
+    bossdoor = new Group();
+    bossdoor.w = tilesize;
+    bossdoor.h = tilesize;
+    bossdoor.color = "crimson";
+    bossdoor.tile = "n";
+    bossdoor.collider = "n";
+
     mapA = new Tiles([
-        "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+    "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
@@ -44,7 +138,7 @@ function maps(){
     "bbbbbbbbbbbbbbbbbbtpppppppppptSSSSSSpppptHHHHHHHHHHHtbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbtpppppppppptHHHHHHpppptHHHHHHHHHHHtbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbtpppppppppptpppppppppptHHHHHHHHHHHtbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-    "bbbbbbbbbbbbbbbbbbtppppprppppDppppppppppDHHHHHHHHHHHDbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+    "bbbbbbbbbbbbbbbbbbtppppprppppDppppppppppDHHHHHOHHHHHDbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbtpppprppppptpppppppppptHHHHHHHHHHHtbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbtpppppppppptpppppppppptHHHHHHHHHHHtbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbtpppppppppptpppppppppptHHHHHHHHHHHtbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
@@ -104,17 +198,56 @@ function maps(){
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
     "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-    ])
+    ],
+    0, 0,
+    tilesize, tilesize
+)
 }
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
 
+    maps();
+
+    player = new Sprite(0,0, tilesize--, tilesize--, "d");
+    player.rotationLock = "true";
+    player.color = "green";
+    
+    origin[0].pos.x;
+    origin[0].pos.y;
 
 } 
 
 function draw(){
     background(0);
+
+    movement();
+
+}
+
+function cameradefiner(){
+
+    camera.x = player.x;
+    camera.y = player.y;
+    camera.zoom = 2;
+
+}
+
+function movement(){
+
+
+    if(kb.pressing("W")){
+        player.y -= 2;
+    }   
+    if(kb.pressing("S")){
+        player.y += 2;
+    }
+    if(kb.pressing("A")){
+        player.x -= 2;
+    }
+    if(kb.pressing("D")){
+        player.x += 2;
+    }
 
 
 }
